4d8b7537c5439832ca2cfbba8a98b2ac188fa5c2
amir8602 <amirreza8602@gmail.com>
2022-02-14 12:22:10 +0330

    end Se2-1

--- src/main/java/com/amir/spring/core/Main.java
+++ src/main/java/com/amir/spring/core/Main.java
@@ -1,7 +1,14 @@
 package com.amir.spring.core;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Main {
     public static void main(String[] args) {
+        ApplicationContext applicationContext =
+                new ClassPathXmlApplicationContext("applicationContext.xml");
+        Student student = (Student) applicationContext.getBean("student");
+        System.out.println(student);
 
     }
 }
---
+++ src/main/java/com/amir/spring/core/Student.java
@@ -0,0 +1,12 @@
+package com.amir.spring.core;
+
+
+import lombok.*;
+
+@Getter@Setter@AllArgsConstructor@NoArgsConstructor@ToString
+
+public class Student {
+    private String name;
+    private String family;
+    private Integer age;
+}
